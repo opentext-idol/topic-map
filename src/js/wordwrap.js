2785cd1001fd85648400f8b7c3f35bd261ae5c50
Alex Scown <alex.scown@hpe.com>
2017-03-29 18:15:16 +0100

    Update copyright

--- src/js/wordwrap.js
+++ src/js/wordwrap.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2016-2017 Hewlett Packard Enterprise Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // We're using AMD, e.g. require.js. Register as an anonymous module.
